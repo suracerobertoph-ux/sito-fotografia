119cc28562b3e0cf120ed7ff5cff17261dbed41d
suracerobertoph-ux <surace.roberto.ph@gmail.com>
2026-01-07 17:57:47 +0100

    Add files via upload

---
+++ gallery_data.js
@@ -0,0 +1,21 @@
+/**
+ * CONFIGURAZIONE GENERATA AUTOMATICAMENTE
+ * Non modificare questo file manualmente.
+ * Usa lo script 'aggiorna_galleria.command' dopo aver aggiunto le foto.
+ */
+
+var GALLERY_DATA = {
+    ritratti: [
+        { src: 'assets/images/ritratti/Untitled 3.jpeg' },
+        { src: 'assets/images/ritratti/Untitled.jpeg' },
+        { src: 'assets/images/ritratti/img20250809_09164261.jpg' },
+        { src: 'assets/images/ritratti/img20251204_11513639.jpg' },
+    ],
+    paesaggio: [
+        { src: 'assets/images/paesaggio/img20250821_09460027 - dimensioni grandi.jpeg' },
+        { src: 'assets/images/paesaggio/img20250909_10452154.jpg' },
+        { src: 'assets/images/paesaggio/img20250929_08550567.jpg' },
+    ],
+    homepage: 'assets/images/homepage/img20250909_10452154.jpg',
+    about: 'assets/images/about/img20250909_09433035.jpg',
+};
