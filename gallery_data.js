99a9c5a5fbc419a858c2398e6b8b286cd635bd71
suracerobertoph-ux <surace.roberto.ph@gmail.com>
2026-01-18 10:24:26 +0100

    Add files via upload

--- gallery_data.js
+++ gallery_data.js
@@ -6,17 +6,12 @@
 
 var GALLERY_DATA = {
     ritratti: [
-        { src: 'assets/images/ritratti/Untitled.jpeg' },
         { src: 'assets/images/ritratti/img20251231_08151600.jpg' },
-        { src: 'assets/images/ritratti/img20260105_07264499.jpg' },
         { src: 'assets/images/ritratti/img20260105_07400959.jpg' },
-        { src: 'assets/images/ritratti/img20260105_07555683.jpg' },
-        { src: 'assets/images/ritratti/img20260105_08045983.jpg' },
     ],
     paesaggio: [
         { src: 'assets/images/paesaggio/img20250821_09460027 - dimensioni grandi.jpeg' },
         { src: 'assets/images/paesaggio/img20250909_10452154.jpg' },
-        { src: 'assets/images/paesaggio/img20250929_08550567.jpg' },
     ],
     homepage: 'assets/images/homepage/img20250909_10452154.jpg',
     about: 'assets/images/about/img20250909_09433035.jpg',
